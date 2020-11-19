9cc02a4d51670a919e05019b0f7bc6b5f9d0fa82
WhiteLady1 <megan15963@gmail.com>
2020-11-19 19:48:53 +0100

    Začínáme

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Logo from './mainPage/Logo';
 import './index.html';
 
-render(
-  <>
-    <header>
-      <h1>React Starter</h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+const App = () => {
+  return (
+    <>
+      <Logo />
+    </>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
---
+++ src/mainPage/Logo/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Logo=()=>{
+  return <h1>BeeScale</h1>
+}
+export default Logo
