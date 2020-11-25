5069523a8bac280bad7008210f693bf43aaf6210
WhiteLady1 <megan15963@gmail.com>
2020-11-25 19:10:37 +0100

    údaje vah, styl

--- src/dashboard/UdajeVaha/UdajeVaha.jsx
+++ src/dashboard/UdajeVaha/UdajeVaha.jsx
@@ -4,21 +4,46 @@ import weightHanging from '@iconify/icons-fa-solid/weight-hanging';
 import tempIcon from '@iconify/icons-raphael/temp';
 import energyIcon from '@iconify/icons-simple-line-icons/energy';
 import humidityIcon from '@iconify/icons-carbon/humidity';
+import './udajeVaha.css';
 
 const UdajeVaha = () => {
   return (
     <>
       <div className="weight">
         <Icon icon={weightHanging} style={{ fontSize: '71px' }} />
+        <p>Hmotnost</p>
+        <p>56</p>
+        <p>Kg</p>
       </div>
       <div className="temperature">
         <Icon icon={tempIcon} style={{ fontSize: '82px' }} />
+        <p>Teplota </p>
+        <div className="temperature-in">
+          <p>in</p>
+          <div className="temperature-in group">
+            <p>25</p>
+            <p>°C</p>
+          </div>
+        </div>
+        <div className="temperature-out">
+          <p>out</p>
+          <div className="temperature-out group">
+            <p>-2</p>
+            <p>°C</p>
+          </div>
+        </div>
       </div>
       <div className="humidity">
         <Icon icon={humidityIcon} style={{ fontSize: '82px' }} />
+        <p>Vlhkost</p>
+        <p>40</p>
+        <p>%</p>
       </div>
       <div className="voltage">
         <Icon icon={energyIcon} style={{ fontSize: '71px' }} />
+        <p>Stav baterie</p>
+        <p>100</p>
+        <p>%</p>
       </div>
     </>
   );
