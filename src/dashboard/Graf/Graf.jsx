52b8b1e8ef19ca9bfdbfebe69578453b611e3e7e
PajaCzechita <p.bajgarova@gmail.com>
2020-11-28 17:05:12 +0100

    add timeRange

--- src/dashboard/Graf/Graf.jsx
+++ src/dashboard/Graf/Graf.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Chart } from 'react-charts';
 import './style.css';
 
+export const parseDateTime = (dateTimeString) => {
+  const [date, time] = dateTimeString.split(' ');
+  const [d, m, y] = date.split('.');
+  const [hour, min] = time.split(':');
+  return new Date(y, parseInt(m) - 1, d, hour, min);
+};
+
 const Graf = (props) => {
+  console.log(props.data[0]);
   const dataChart = [
     {
-      label: 'Series 1',
+      label: 'Váha 1',
       data: [],
     },
   ];
@@ -13,9 +21,9 @@ const Graf = (props) => {
   const dataKNaplneni = dataChart[0].data;
 
   props.data.forEach((udaj) => {
-    console.log(new Date(udaj.time));
+    //console.log(new Date(udaj.time));
     dataKNaplneni.push({
-      primary: new Date(udaj.time),
+      primary: parseDateTime(udaj.time),
       secondary: udaj.weight,
     });
   });
@@ -32,9 +40,15 @@ const Graf = (props) => {
     [],
   );
   return (
-    <div className="graf">
-      <Chart data={dataChart} axes={axes} tooltip />
-    </div>
+    <>
+      <button onClick={() => props.setTimeOffset(24)}>24h</button>
+      <button onClick={() => props.setTimeOffset(48)}>48h</button>
+      <button onClick={() => props.setTimeOffset(24 * 7)}>7d</button>
+      <button onClick={() => props.setTimeOffset(24 * 30)}>30d</button>
+      <div className="graf">
+        <Chart data={dataChart} axes={axes} tooltip />
+      </div>
+    </>
   );
 };
 export default Graf;
