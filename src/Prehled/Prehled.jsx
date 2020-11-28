874d6e7a8918251ab51431b824365744e1231200
WhiteLady1 <megan15963@gmail.com>
2020-11-28 19:30:42 +0100

    Přehled - ikony

--- src/Prehled/Prehled.jsx
+++ src/Prehled/Prehled.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
+import weightHanging from '@iconify/icons-fa-solid/weight-hanging';
+import tempIcon from '@iconify/icons-raphael/temp';
+import energyIcon from '@iconify/icons-simple-line-icons/energy';
+import humidityIcon from '@iconify/icons-carbon/humidity';
 
 const Prehled = () => {
   return (
     <>
-      <div>Jsem přehled koukej :)</div>
+      <div>Jsem ultra mega vytuněnej přehled :)</div>
+      <div className="prehledVah">
+        <div className="prehledVahy">
+          <div className="hlavniPrehled">
+            <div className="jmenoVaha">Moje váha</div>
+            <Icon icon={weightHanging} style={{ fontSize: '71px' }} />
+            <div className="hmotnost">56</div>
+            <div className="jednotka">kg</div>
+          </div>
+          <div className="vedlejsiPrehled">
+            <div className="hodnoty">
+              <Icon icon={tempIcon} style={{ fontSize: '82px' }} />
+              <div className="hodnota">56</div>
+              <div className="jednotka">°C</div>
+            </div>
+            <div className="hodnoty">
+              <Icon icon={humidityIcon} style={{ fontSize: '82px' }} />
+              <div className="hodnota">56</div>
+              <div className="jednotka">°C</div>
+            </div>
+            <div className="hodnoty">
+              <Icon icon={humidityIcon} style={{ fontSize: '82px' }} />
+              <div className="hodnota">56</div>
+              <div className="jednotka">%</div>
+            </div>
+            <div className="baterie">
+              <Icon icon={energyIcon} style={{ fontSize: '71px' }} />
+              <div className="hodnota">56</div>
+              <div className="jednotka">kg</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
