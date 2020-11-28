f2dcdb556bb75e41a24ef6d9ee3d087bede3ae53
PajaCzechita <p.bajgarova@gmail.com>
2020-11-28 11:42:45 +0100

    graf v2

--- src/dashboard/Graf/Graf.jsx
+++ src/dashboard/Graf/Graf.jsx
@@ -1,54 +1,53 @@
 import React from 'react';
 import { Chart } from 'react-charts';
-
+import './style.css';
 const data = [
   {
     label: 'Series 1',
     data: [
       {
-        primary: '2020-11-28T09:00:00.000Z',
+        primary: new Date('2020-11-28T09:00:00.000Z'),
         secondary: 58,
       },
       {
-        primary: '2020-11-29T09:00:00.000Z',
+        primary: new Date('2020-11-29T09:00:00.000Z'),
         secondary: 84,
       },
       {
-        primary: '2020-11-30T09:00:00.000Z',
+        primary: new Date('2020-11-30T09:00:00.000Z'),
         secondary: 34,
       },
       {
-        primary: '2020-12-01T09:00:00.000Z',
+        primary: new Date('2020-12-01T09:00:00.000Z'),
         secondary: 13,
       },
       {
-        primary: '2020-12-02T09:00:00.000Z',
+        primary: new Date('2020-12-02T09:00:00.000Z'),
         secondary: 75,
       },
       {
-        primary: '2020-12-03T09:00:00.000Z',
+        primary: new Date('2020-12-03T09:00:00.000Z'),
         secondary: 15,
       },
       {
-        primary: '2020-12-04T09:00:00.000Z',
+        primary: new Date('2020-12-04T09:00:00.000Z'),
         secondary: 11,
       },
       {
-        primary: '2020-12-05T09:00:00.000Z',
+        primary: new Date('2020-12-05T09:00:00.000Z'),
         secondary: 7,
       },
       {
-        primary: '2020-12-06T09:00:00.000Z',
+        primary: new Date('2020-12-06T09:00:00.000Z'),
         secondary: 40,
       },
       {
-        primary: '2020-12-07T09:00:00.000Z',
+        primary: new Date('2020-12-07T09:00:00.000Z'),
         secondary: 74,
       },
     ],
   },
 ];
-
 const Graf = () => {
   const axes = React.useMemo(
     () => [
@@ -56,14 +55,15 @@ const Graf = () => {
         primary: true,
         type: 'time',
         position: 'bottom',
-        // filterTicks: (ticks) =>
-        //   ticks.filter((date) => +timeDay.floor(date) === +date),
       },
       { type: 'linear', position: 'left' },
     ],
     [],
   );
-
-  return <Chart data={data} axes={axes} tooltip />;
+  return (
+    <div className="graf">
+      <Chart data={data} axes={axes} tooltip />
+    </div>
+  );
 };
 export default Graf;
