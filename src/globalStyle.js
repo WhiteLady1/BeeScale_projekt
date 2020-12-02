e47dfd8cd2bd30b175cd56fe88e12c0bf237097a
WhiteLady1 <megan15963@gmail.com>
2020-12-02 17:30:21 +0100

    Globální barva  odkazu blanka a decor. none

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -3,6 +3,10 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
 body{
   font-family: 'Open Sans', sans-serif;
+}
+a{
+  text-decoration:none;
+  color: black
 }`;
 
 export default GlobalStyle;
